d936d7589196200868cb040d02d8f390991d8fa4
dddddyk <2925744675@qq.com>
2026-07-20 11:09:09 +0800

    单个图片完成

--- Assets/Scripts/BaseImage.cs
+++ Assets/Scripts/BaseImage.cs
@@ -7,13 +7,25 @@ public class BaseImage : MonoBehaviour
     [Header("下落速度")]
     public float fallspeed = 300f;
     private RectTransform rect;
+    private float parentHeight;
+    private float myHeight;
+    private RectTransform parentRect;
     void Start()
     {
         rect = GetComponent<RectTransform>();
+        
+        parentRect = transform.parent as RectTransform;
+        parentHeight = parentRect.rect.height;
+        myHeight = rect.rect.height; // 获取自己卡片的高度
+
     }
     void Update()
     {
         rect.anchoredPosition -= new Vector2(0, fallspeed * Time.deltaTime);
+        if (rect.anchoredPosition.y <= -parentHeight / 2 + -myHeight / 2)
+        {
+            rect.anchoredPosition = new Vector2(0, parentHeight / 2 + myHeight / 2);
+        }
     }
 
 
